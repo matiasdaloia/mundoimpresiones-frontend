b98f9636a7eb4651079bb0998cb2a74cd00d754a
matiasdaloia <matidaloia@gmail.com>
2020-10-20 18:50:25 -0300

    finished product pages

--- src/templates/productTemplate.js
+++ src/templates/productTemplate.js
@@ -1,13 +1,16 @@
 import React from "react"
+import { Container } from "react-bootstrap"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
+import styled from "styled-components"
+import { FaPaypal } from "react-icons"
 
 export const query = graphql`
   query($slug: String!) {
     allStrapiProductos(filter: { slug: { eq: $slug } }) {
       edges {
         node {
+          id
           slug
           titulo
           precio
@@ -28,32 +31,96 @@ export const query = graphql`
   }
 `
 
+const Row = styled.div`
+  display: flex;
+  @media (max-width: 900px) {
+    flex-direction: column;
+
+    & > img {
+      width: 100%;
+    }
+  }
+`
+
+const ProductDetails = styled.div`
+  padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 900px) {
+    padding: 0;
+  }
+`
+
+const Categoria = styled.h5`
+  background-color: #6fc2b8;
+  color: white;
+  width: fit-content;
+  border-radius: 20px;
+  padding: 2px 5px;
+  font-size: 0.8rem;
+  margin-top: 1rem;
+`
+
+const Precio = styled.h4`
+  font-size: 1.5rem;
+  background-color: #ef476f;
+  color: white;
+  border-radius: 10px;
+  padding: 0.5rem;
+  width: fit-content;
+`
+
+const Descripcion = styled.p`
+  font-size: 1.2rem;
+  color: gray;
+  margin-top: 1rem;
+`
+
+const Button = styled.button`
+  background-color: #ef476f;
+  color: white;
+  border-radius: 10px;
+  border: none;
+  padding: 0.5rem;
+  display: block;
+  width: 100%;
+  outline: none;
+  margin-top: 1rem;
+  :hover {
+    background-color: #6fc2b8;
+  }
+`
+
 function ProductTemplate({ data }) {
-  console.log(
-    data.allStrapiProductos.edges[0].node.imagen.childImageSharp.fluid.src
-  )
   return (
     <Layout>
-      <h1>{data.allStrapiProductos.edges[0].node.titulo}</h1>
-      <p>{data.allStrapiProductos.edges[0].node.descripcion}</p>
-      <Img
-        fluid={
-          data.allStrapiProductos.edges[0].node.imagen.childImageSharp.fluid
-        }
-        style={{ width: "200px" }}
-      />
-      <button
-        className="snipcart-add-item"
-        data-item-id={data.allStrapiProductos.edges[0].node.id}
-        data-item-price="80"
-        data-item-url={`/product/${data.allStrapiProductos.edges[0].node.slug}`}
-        data-item-name="curso"
-        data-item-image={
-          data.allStrapiProductos.edges[0].node.imagen.childImageSharp.fluid.src
-        }
-      >
-        COMPRAR
-      </button>
+      <Container>
+        <Row>
+          <img
+            src={
+              data.allStrapiProductos.edges[0].node.imagen.childImageSharp.fluid
+                .src
+            }
+          />
+          <ProductDetails>
+            <h1>{data.allStrapiProductos.edges[0].node.titulo}</h1>
+            <Categoria>
+              {data.allStrapiProductos.edges[0].node.categorias.nombre}
+            </Categoria>
+
+            <Precio>$25</Precio>
+
+            <Descripcion>
+              {data.allStrapiProductos.edges[0].node.descripcion}
+            </Descripcion>
+
+            <div>
+              <Button>COMPRAR</Button>
+            </div>
+          </ProductDetails>
+        </Row>
+      </Container>
     </Layout>
   )
 }
