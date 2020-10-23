47cfd09799e6e20318ae5027f3533aa70474b6e0
matiasdaloia <matidaloia@gmail.com>
2020-10-23 10:15:10 -0300

    changed header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./header.css"
-import { FaInstagram, FaFacebook } from "react-icons/fa"
+import { FaInstagram, FaFacebook, FaSearch } from "react-icons/fa"
 import { Link } from "gatsby"
 import "bootstrap/dist/css/bootstrap.css"
 import { Navbar, Nav } from "react-bootstrap"
@@ -14,8 +14,8 @@ const Header = () => {
             className="logo"
             src="https://scontent.fros2-1.fna.fbcdn.net/v/t1.0-9/94257966_2506847026082868_2526139269169283072_o.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=F1ncPNI-hDAAX9Odb8Z&_nc_ht=scontent.fros2-1.fna&oh=0c5acf0b583cc1330e8276d3c9670e02&oe=5FB52B43"
             alt="logo"
-            width="50"
-            height="50"
+            width="80"
+            height="80"
           />
         </Link>
       </Navbar.Brand>
@@ -26,6 +26,12 @@ const Header = () => {
           <Link to="/descargables">DESCARGABLES</Link>
           <Link to="/#kits">KITS IMPRIMIBLES</Link>
           <Link to="/blog">BLOG</Link>
+          <div className="header__leftSearch">
+            <input type="text" placeholder="Buscar" />
+            <div>
+              <FaSearch />
+            </div>
+          </div>
         </Nav>
         <Nav className="ml-auto header__right">
           <a href="https://instagram.com/mundo_imp" target="_blank">
